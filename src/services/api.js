c09d3d25690c38b6b90f15b5bfd51ee632240a30
Bagrielzin <gabriel.kratos2002@gmail.com>
2025-10-28 20:04:12 -0300

    feat: adiciona link do baackend deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://cinelist-backend-qj4c.onrender.com', // local: http://localhost:8000
   headers: {
     'Content-Type': 'application/json',
   },
